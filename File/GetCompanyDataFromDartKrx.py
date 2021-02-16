adb18d839bbc6458317740c621e3d3f4ce80d41d
지상열 <jisang-yeol@jisang-yeol-ui-MacBookPro.local>
2021-02-16 21:30:11 +0900

    KRX data parsing

--- File/GetCompanyDataFromDartKrx.py
+++ File/GetCompanyDataFromDartKrx.py
@@ -1,6 +1,6 @@
-#_*_ coding: utf-8 _*_
 import pandas
 import requests
+import io
 
 #상장주식수 파싱하기위해서 아래 URL에서 얻어옴..
 #GenerateOTP에서 OTP를 받아오고 download url에서 code의 value로 넣어줌
@@ -13,7 +13,7 @@ class GetCompanyDataFromDartKrx:
         "name": "fileDown",\
         "share": "1",\
         "url": "dbms/MDC/STAT/standard/MDCSTAT01901"}
-    __DOWNURL = "http://data.krx.co.kr/comm/fileDn/download_csv/download.cmd"
+    __DOWNURL = "http://data.krx.co.kr/comm/fileDn/download_csv/download.cmd"#Response string is CSV format
     __DOWNPOSTDATA ={'code': ''}
 
     def __init__(self):
@@ -21,7 +21,14 @@ class GetCompanyDataFromDartKrx:
         GetCompanyDataFromDartKrx.__DOWNPOSTDATA['code'] = genRes.text
         downRes = requests.post(GetCompanyDataFromDartKrx.__DOWNURL,data = GetCompanyDataFromDartKrx.__DOWNPOSTDATA)
         downRes.encoding = 'euc-kr'
-        print(downRes.text)
+        companyDf = pandas.read_csv(io.StringIO(downRes.text))#downRes.text is not a file so we should add the IO formatter
+        #표준코드,단축코드,한글 종목명,한글 종목약명,영문 종목명,상장일,시장구분,증권구분,소속부,주식종류,액면가,상장주식수
+        for i in companyDf.index:
+            print(companyDf.loc[i, '단축코드'])
+            print(companyDf.loc[i, '시장구분'])
+            print(companyDf.loc[i, '액면가'])
+            print(companyDf.loc[i, '상장주식수'])
+            
 
 if __name__ == '__main__':
     parsedData = GetCompanyDataFromDartKrx()
